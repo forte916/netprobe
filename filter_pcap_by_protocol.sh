53bc101da37700bf6d0a99be792b7db7150a4dad
Shinichi Noma <fortissimo916@gmail.com>
2016-08-04 19:50:37 +0900

    enhance output format.

--- filter_pcap_by_protocol.sh
+++ filter_pcap_by_protocol.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#
+# - Filter pcap by each protocols.
+# - Not support ipv6 currently.
+#
+
 if [ "$1" == "" ]; then
   echo "usage: $0 pcap_file_name"
   exit 1
@@ -8,46 +13,54 @@ INPUT="$1"
 
 
 echo ""
+echo "===== DNS ====="
+echo "| src ip | dst ip | dst port | dns qery name |"
+FILTER="dns.flags.response == 0"
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e udp.dstport -e dns.qry.name | sort | uniq
+
+
+echo ""
 echo "===== http ====="
-echo "| src ip | dst ip | dst port | dst host |"
-FILTER="http"tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport ip.dst_host | sort | uniq
+echo "| src ip | dst ip | dst port | hostname | url |"
+FILTER="http.request"
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport -e http.host -e http.request.uri | sort | uniq
 
 
 echo ""
 echo "===== ssl ====="
-echo "| src ip | dst ip | dst port | dst host |"
-FILTER="ssl"
-tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport ip.dst_host | sort | uniq
+echo "| src ip | dst ip | dst port | ssl version | hostname |"
+FILTER="ssl.handshake.type == 1"
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport -e ssl.record.version -e ssl.handshake.extensions_server_name | sort | uniq
 
 
 echo ""
 echo "===== Other tcp ====="
-echo "| src ip | dst ip | dst port | dst host |"
+echo "| src ip | dst ip | protocol |"
 FILTER="tcp && !http && !ssl"
-tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport ip.dst_host | sort | uniq
-
-
-echo ""
-echo "===== DNS ====="
-echo "| src ip | dst ip | dst port | dst host |"
-FILTER="dns"
-tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport ip.dst_host | sort | uniq
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e frame.protocols | sort | uniq
 
 
 echo "" 
 echo "===== QUIC ====="
-echo "| src ip | dst ip | dst port | dst host |"
-FILTER="quic"
-tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport ip.dst_host | sort | uniq
+echo "| src ip | dst ip | dst port | quic version | hostname |"
+FILTER="quic.tag"
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e udp.dstport -e quic.version -e quic.tag.sni | sort | uniq
 
 
 echo ""
 echo "===== Other UDP ====="
-echo "| src ip | dst ip | dst port | dst host |"
-FILTER="udp && !dns && !quic"
-tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e tcp.dstport ip.dst_host | sort | uniq
+echo "| src ip | dst ip | dst port | protocol |"
+FILTER="udp && !dns && !quic && !icmp"
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e frame.protocols | sort | uniq
 
+
+echo ""
+echo "===== ICMP ====="
+echo "| src ip | dst ip | dst port | protocol |"
+FILTER="icmp"
+tshark -2 -V -n -r "$1" -R "$FILTER" -T fields -e ip.src -e ip.dst -e frame.protocols | sort | uniq
 
 
 
 echo "===== Finished ====="
+
